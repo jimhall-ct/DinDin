5cb6e948b2b76eb8f72a84d50c055593c658a700
Jim Hall <jim.hall@solarmedia.com>
2017-05-08 16:34:12 -0400

    Add ability to star recipes

--- index.js
+++ index.js
@@ -4,15 +4,46 @@ const Sqlite3 = require('sqlite3');
 const server = new Hapi.Server();
 const db = new Sqlite3.Database('./dindin.sqlite');
 
-server.bind({ db: db });
+server.bind({db: db});
 
 server.connection({
-  host: 'localhost',
-  port: 8080
+    host: 'localhost',
+    port: 8080
 });
 
-server.route(require('./routes'));
+const validateFunc = function (token, callback) {
+    db.get('SELECT * FROM users  WHERE token = ?',
+        [token],
+        (err, result) => {
 
-server.start(function(err) {
-  console.log('Listening at:', server.info.uri);
+        if (err) {
+            return callback(err, false);
+        }
+
+        const user = result;
+
+        if (typeof user === 'undefined') {
+            return callback(null, false);
+        }
+
+        callback(null, true, {
+            id: user.id,
+            username: user.username
+        });
+    });
+};
+
+server.register(require('hapi-auth-bearer-token'), (err) => {
+    if (err) throw err;
+
+    server.auth.strategy('api', 'bearer-access-token', {
+        validateFunc: validateFunc
+    });
+
+    server.route(require('./routes'));
+
+    server.start(function (err) {
+        console.log('Listening at:', server.info.uri);
+    });
 });
+
--- routes.js
+++ routes.js
@@ -1,36 +1,29 @@
 'use strict';
 
+const Recipes = require('./handlers/recipes');
+
 module.exports = [
     {
         method: 'GET',
         path: '/api/v1/recipes',
-        handler: function (request, reply) {
-          let sql = 'SELECT * FROM recipes';
-          const params = [];
-          if (request.query.cuisine) {
-            console.log(request.query.cuisine);
-            sql += ' WHERE cuisine = ?';
-            params.push(request.query.cuisine);
-          }
-          db.all(sql, params, (err, results) => {
-            if (err) throw err;
-            reply(results);
-          });
-        }
-    },{
+        handler: Recipes.find
+    }, {
         method: 'GET',
         path: '/api/v1/recipes/{id}',
-        handler: function (request, reply) {
-          let sql = 'SELECT * FROM recipes WHERE id = ?';
-          db.get('SELECT * FROM recipes WHERE id = ?',
-              [request.params.id],
-              (err, results) => {
-                  if (err) throw err;
-                  if (typeof results !== 'undefined') {
-                      return reply(results);
-                  }
-                  return reply('Not Found').code(404);
-          });
-        }
+        handler: Recipes.findOne
+    }, {
+        method: 'POST',
+        path: '/api/v1/recipes',
+        config: {
+          auth: 'api'
+        },
+        handler: Recipes.create
+    }, {
+        method: 'POST',
+        path: '/api/v1/recipes/{id}/star',
+        config: {
+          auth: 'api'
+        },
+        handler: Recipes.star
     }
 ];
